cac08a1c3e26c2665fe7818d0dabbdb710c336e0
DaytraderTools <tk@daytradertools.de>
2026-03-13 08:29:45 +0100

    Update Lightbox plugin to Mantis v2.28.0
    Refactor lightbox functionality to apply to image previews and links.

--- files/Lightbox.js
+++ files/Lightbox.js
@@ -1,12 +1,35 @@
 (function ($) {
   $(document).ready(function () {
-    if (lightbox_display_on_img_preview) {
-      $('.bug-attachment-preview-image a').attr('rel', 'lightbox');
+
+    // Standardwerte, falls du (vorerst) keine Plugin-Konfiguration nutzt
+    var lightbox_display_on_img_preview = true;
+    var lightbox_display_on_img_link    = true;
+
+    function applyLightbox() {
+        // 1. Vorschaubilder in der Hauptansicht (Anhänge mit Bild)
+        if (lightbox_display_on_img_preview) {
+            $('a[href*="file_download.php"]').filter(function() {
+                return $(this).find('img').length > 0;
+            }).attr('data-lightbox', 'bug-attachments');
+        }
+
+        // 2. Links in Bugnotes/Kommentaren
+        if (lightbox_display_on_img_link) {
+            $('a[href*="file_download.php"]').each(function() {
+                var href = $(this).attr('href') || '';
+                if (href.match(/\.(jpeg|jpg|gif|png|webp)$/i)) {
+                    $(this).attr('data-lightbox', 'bug-attachments');
+                }
+            });
+        }
     }
-    if (lightbox_display_on_img_link) {
-      $('div#bugnotes').find('*').filter(function() {
-        return this.id.match(/attachment_preview_\d+_(open|closed)/);
-      }).find('a:nth-child(2)').attr('rel', 'lightbox');
-    }
+
+    applyLightbox();
+
+    // Falls Inhalte per AJAX nachgeladen werden
+    $(document).ajaxComplete(function() {
+        applyLightbox();
+    });
+
   });
 })(jQuery);
