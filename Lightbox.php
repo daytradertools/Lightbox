058baec32866cf8f386f7916ccb532548db96430
DaytraderTools <tk@daytradertools.de>
2026-03-13 08:28:58 +0100

    Update Lightbox plugin to Mantis v2.28.0

--- Lightbox.php
+++ Lightbox.php
@@ -20,20 +20,23 @@ require_api( 'crypto_api.php' );
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  * or see http://www.gnu.org/licenses/.
  */
+ 
 class LightboxPlugin extends MantisPlugin {
 
     function register() {
-        $this->name = plugin_lang_get('title');
-        $this->description = plugin_lang_get('description');
-        $this->version = '1.0';
-        $this->page = 'config_page';
-        $this->requires = array(
-            'MantisCore' => '2.0.0',
+        $this->name        = plugin_lang_get( 'title' );
+        $this->description = plugin_lang_get( 'description' );
+        $this->version     = '1.1.1';
+        $this->page        = 'config_page';
+        $this->requires    = array(
+            'MantisCore' => '2.28.0',
         );
-        $this->author = 'Karim Ratib';
-        $this->contact = 'karim@meedan.com';
-        $this->url = 'http://code.meedan.com';
-        $this->nonce = crypto_generate_uri_safe_nonce( 16 );
+        $this->author  = 'T. K. (angepasst)';
+        $this->contact = 'info@daytradertools.de';
+        $this->url     = 'http://daytradertools.de';
+
+        # Nonce für Inline-Scripts (CSP)
+        $this->nonce   = crypto_generate_uri_safe_nonce( 16 );
     }
 
     /**
@@ -42,63 +45,68 @@ class LightboxPlugin extends MantisPlugin {
     function config() {
         return array(
             'display_on_img_preview' => ON,
-            'display_on_img_link' => ON,
+            'display_on_img_link'    => ON,
         );
     }
 
+    /**
+     * Registrierte Hooks
+     */
     function hooks() {
         return array(
             'EVENT_LAYOUT_RESOURCES' => 'add_lightbox',
-            'EVENT_CORE_HEADERS' => 'csp_headers',
+            'EVENT_CORE_HEADERS'     => 'csp_headers',
         );
     }
 
-    function add_lightbox($event) {
-        $currentUrl = explode('/', $_SERVER['PHP_SELF']);
-        if (end($currentUrl) !== 'view.php') return;
+    /**
+     * Ressourcen (CSS/JS) für Lightbox hinzufügen.
+     * Wird auf jeder Seite eingebunden, aber das JS selbst
+     * filtert auf passende Links (file_download.php).
+     */
+    function add_lightbox( $p_event ) {
+        $t_lightbox_display_on_img_preview = plugin_config_get( 'display_on_img_preview' );
+        $t_lightbox_display_on_img_link    = plugin_config_get( 'display_on_img_link' );
 
-        $lightbox_display_on_img_preview = plugin_config_get('display_on_img_preview');
-        $lightbox_display_on_img_link = plugin_config_get('display_on_img_link');
-        $lightboxJs = plugin_file('lightbox/js/lightbox.min.js');
-        $lightboxCss = plugin_file('lightbox/css/lightbox.min.css');
-        $lightboxMantis = plugin_file('Lightbox.js');
-        $images = [
-          'prev' => plugin_file('lightbox/img/prev.png'),
-          'next' => plugin_file('lightbox/img/next.png'),
-          'close' => plugin_file('lightbox/img/close.png'),
-          'loading' => plugin_file('lightbox/img/loading.gif'),
-        ];
+        $t_lightbox_js   = plugin_file( 'lightbox/js/lightbox.min.js' );
+        $t_lightbox_css  = plugin_file( 'lightbox/css/lightbox.min.css' );
+        $t_mantis_js     = plugin_file( 'Lightbox.js' );
+
+        $t_images = array(
+            'prev'    => plugin_file( 'lightbox/img/prev.png' ),
+            'next'    => plugin_file( 'lightbox/img/next.png' ),
+            'close'   => plugin_file( 'lightbox/img/close.png' ),
+            'loading' => plugin_file( 'lightbox/img/loading.gif' ),
+        );
+
+        $t_nonce = $this->nonce;
 
         return <<<LIGHTBOX
-<script type="text/javascript" nonce={$this->nonce}>
-var lightbox_display_on_img_preview = {$lightbox_display_on_img_preview};
-var lightbox_display_on_img_link = {$lightbox_display_on_img_link};
-</script>
-<link href="{$lightboxCss}" rel="stylesheet">
-<script type="text/javascript" src="{$lightboxJs}"></script>
-<script type="text/javascript" src="{$lightboxMantis}"></script>
-<style>
-.lb-nav a.lb-prev {
-  background-image: url({$images['prev']});
-}
-.lb-nav a.lb-next {
-  background-image: url({$images['next']});
-}
-.lb-data .lb-close {
-  background-image: url({$images['close']});
-}
-.lb-cancel {
-  background-image: url({$images['loading']});
-}
+
+<link rel="stylesheet" type="text/css" href="{$t_lightbox_css}" />
+
+<script type="text/javascript" src="{$t_lightbox_js}"></script>
+<script type="text/javascript" src="{$t_mantis_js}"></script>
+
+<style type="text/css">
+    .lb-nav a.lb-prev {
+        background-image: url({$t_images['prev']});
+    }
+    .lb-nav a.lb-next {
+        background-image: url({$t_images['next']});
+    }
+    .lb-data .lb-close {
+        background-image: url({$t_images['close']});
+    }
+    .lb-cancel {
+        background-image: url({$t_images['loading']});
+    }
 </style>
 LIGHTBOX;
     }
 
-    /**
-     * Add Content Security Policy headers for our script.
-     */
+    
     function csp_headers() {
-       http_csp_add( 'script-src', "'nonce-{$this->nonce}'" );
-       http_csp_add( 'img-src', "data:" );
+        //Removed
     }
 }
